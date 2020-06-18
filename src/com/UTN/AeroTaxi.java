8f9264d103e7bb19e38ea7725a6f2ac1f63e1e3e
Leandro <leandro.nahuel.pintos@gmail.com>
2020-06-18 03:05:40 -0300

    agregue 2 metodos mas pa

--- src/com/UTN/AeroTaxi.java
+++ src/com/UTN/AeroTaxi.java
@@ -6,4 +6,11 @@ import java.util.HashSet;
 public class AeroTaxi {
     private ArrayList<Avion> listaAviones;
     private HashSet<Cliente> listaUsuarios;
+
+    private String pelado;
+    private int edad;
+
+    public void sacarEdaddelPela(){
+
+    }
 }
