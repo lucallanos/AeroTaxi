dd8d3afeff7dd51bf388143392ac4bbe29867806
lucallanos <llanosluca1998@gmail.com>
2020-06-19 01:07:57 -0300

    Subo la nueva clase Menu y los 3 métodos de la clase LogIn

--- src/com/UTN/LogIn.java
+++ src/com/UTN/LogIn.java
@@ -1,4 +1,47 @@
 package com.UTN;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class LogIn {
+
+    Scanner scan = new Scanner(System.in);
+
+    //Recibimos la lista de usuarios registrados para poder comparar con el dni
+    //que se ingresa por teclado, si coincide el usuario ya se encontraba registrado,
+    //sino se tendrá que registrar
+    public boolean ingresar(int dni, ArrayList<Persona> listaCliente){
+        boolean flag = false;
+        for(Persona p: listaCliente){
+            if(p.getDni() == dni){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    //Si se ingresa el "DNI" que establecimos para el admin se ingresara al menú del admin
+    public boolean ingresarAdmin(int dni){
+        boolean flag = false;
+        if(dni == 1905){
+            flag = true;
+        }
+        return flag;
+    }
+
+    public Persona registrarse(){
+        Persona persona = null;
+        System.out.println("-Ingrese su nombre: ");
+        persona.setNombre(scan.nextLine());
+        System.out.println("\n-Ingrese su apellido: ");
+        persona.setApellido(scan.nextLine());
+        System.out.println("\n-Ingrese su DNI:");
+        persona.setDni(scan.nextInt());
+        System.out.println("\n-Ingrese su edad: ");
+        persona.setEdad(scan.nextInt());
+
+        return persona;
+    }
 }
+
+
--- src/com/UTN/Persona.java
+++ src/com/UTN/Persona.java
@@ -6,4 +6,46 @@ public abstract class Persona{
     private int dni;
     private int edad;
 
+    //Constructores
+    Persona(){}
+
+    Persona(String nombre, String apellido, int dni, int edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    //Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni){
+        this.dni = dni;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
